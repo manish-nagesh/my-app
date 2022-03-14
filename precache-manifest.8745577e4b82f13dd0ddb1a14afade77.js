8bf88f708a16e220cd69c21827c83586d1da5032
promanish <manishnagesh0502@gmail.com>
2022-03-14 22:17:00 +0530

    Updates

---
+++ precache-manifest.8745577e4b82f13dd0ddb1a14afade77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee97d441fcde2fce9ad",
+    "url": "/my-app/static/css/main.ae93a7e8.chunk.css"
+  },
+  {
+    "revision": "aee97d441fcde2fce9ad",
+    "url": "/my-app/static/js/main.aee97d44.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/my-app/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/my-app/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "a03fd468ae3d1fa60fff",
+    "url": "/my-app/static/js/runtime~main.a03fd468.js"
+  },
+  {
+    "revision": "97606ddeeaac8bb0dae4f94439333a94",
+    "url": "/my-app/index.html"
+  }
+];
